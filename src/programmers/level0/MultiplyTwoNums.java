cca3a65f2e47cd99c9971f67c56c62f31c36a9ee
kimhanshin4 <funold1004@gmail.com>
2023-11-01 23:30:41 +0900

    두 수의 곱

---
+++ src/programmers/level0/MultiplyTwoNums.java
@@ -0,0 +1,17 @@
+package programmers.level0;
+
+//정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+public class MultiplyTwoNums {
+
+    //public int solution(int num1, int num2) {
+    //        int answer = 0;
+    //        return answer;
+    //    }
+    public static void main(String[] args) {
+        int answer;
+        int num1 = 3;
+        int num2 = 4;
+        answer = num1 * num2;
+        System.out.println(answer);
+    }
+}
