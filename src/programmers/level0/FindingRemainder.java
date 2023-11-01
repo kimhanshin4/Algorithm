eb1bd0c953bae2c047d5c32202ff29af5e89452d
kimhanshin4 <funold1004@gmail.com>
2023-11-01 23:40:08 +0900

    나머지 구하기

---
+++ src/programmers/level0/FindingRemainder.java
@@ -0,0 +1,17 @@
+package programmers.level0;
+
+public class FindingRemainder {
+
+    //정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+    //    public int solution(int num1, int num2) {
+    //        int answer = -1;
+    //        return answer;
+    //    }
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 2;
+        int answer;
+        answer = num1 % num2;
+        System.out.println(answer);
+    }
+}
