0f6c38d6ab61bf8e3583d7481e1efec1d00c9ddd
kimhanshin4 <funold1004@gmail.com>
2023-11-16 12:42:48 +0900

    이상한 문자 만들기 but 이해하기

--- src/programmers/level1/CreateStrangeCharacters.java
+++ src/programmers/level1/CreateStrangeCharacters.java
@@ -9,16 +9,61 @@ public class CreateStrangeCharacters {
     //문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
     //첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
     public static String solution(String s) {
-        String answer = "";
-        String evenC = "";
-        String OddC = "";
-        
+        String answer;
+        String[] wordList = s.split("(?<=\\s)"); //문자열을 단어별로 쪼개기
+        for (int i = 0; i < wordList.length; i++) {
+            String[] wordSplit = wordList[i].split(""); //단어를 문자로 쪼개기
+            for (int j = 0; j < wordSplit.length; j++) {
+                if (j % 2 == 0) { //짝수자리는 대문자로
+                    wordSplit[j] = wordSplit[j].toUpperCase();
+                } else { //나머지인 홀수자리는 대문자로
+                    wordSplit[j] = wordSplit[j].toLowerCase();
+                }
+            }
+            wordList[i] = String.join("", wordSplit); //쪼개진 문자들 단어로 합하기
+        }
+        answer = String.join("", wordList); //단어들 한 줄로 합하기
         return answer;
     }
 
     public static void main(String[] args) {
-        String s = "try hello world";
+//        String s = "try hello world";
+        String s = "what the hell is happening";
+//        String s = "AA aa ZZ zz";
         String answer = solution(s);
-        System.out.println(answer);//"TrY HeLlO WoRlD"
+        System.out.println(answer); //"TrY HeLlO WoRlD"
     }
 }
+//첫 풀이 but error => 단어별 짝/홀수 인덱스를 판단해야함
+//    public String solution(String s) {
+//        List<String> charList = new ArrayList<>();
+//        List<String> charUList = new ArrayList<>();
+//        //char[] charListArr = new char[s.length()];
+//        String answer = "";
+//        //String evenC = "";
+//        //String OddC = "";
+//        for (int i = 0; i < s.length(); i++) {
+//            charList.add(String.valueOf(s.charAt(i)));
+//            ; //문자열s를 문자로 나눠 리스트에 대입
+//        }
+//        //System.out.println(charList.toString());
+//        //System.out.println("-----------------");
+//                for (int i = 0; i < charList.size(); i++) {
+//            if (charList.get(i).matches("[a-zA-Z]")) {
+//                if (i % 2 == 0) {
+//                    charUList.add(charList.get(i).toUpperCase());
+//                } else {
+//                    charUList.add(charList.get(i).toLowerCase());
+//                }
+//            } else {
+//                charUList.add(charList.get(i));
+//            }
+//        }
+////        System.out.println(charUList.toString());
+////        System.out.println("-----------------");
+//        for (int i = 0; i < charList.size(); i++) {
+//            answer = String.join("", charUList);
+//        }
+//
+//        return answer;
+//    }
