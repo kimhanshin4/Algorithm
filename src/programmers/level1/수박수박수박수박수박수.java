d6d582bb4a53dc850a2112d481c51781b8811de8
kimhanshin4 <funold1004@gmail.com>
2023-11-30 10:00:23 +0900

    feat: 수박수박수박수박수박수? //ing ㅠㅠ

---
+++ src/programmers/level1/수박수박수박수박수박수.java
@@ -0,0 +1,25 @@
+package programmers.level1;
+
+import java.util.*;
+
+public class 수박수박수박수박수박수 {
+
+    //수박수박수박수박수박수?
+//길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+// 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+    //제한 조건
+    //n은 길이 10,000이하인 자연수입니다.
+    public static String solution(int n) {
+        String answer = "";
+        int[] subakSubak = new int[n];
+        List<String> subakSubak = new ArrayList<>();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        String answer = solution(n);
+        System.out.println("answer = " + answer);
+    }
+}
