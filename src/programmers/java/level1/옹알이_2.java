e20df3c53832d5bcf79c9ecb81d3fb86305d9da4
kimhanshin4 <funold1004@gmail.com>
2024-01-03 09:54:09 +0900

    YES : 옹알이 (2) - 이것도

--- src/programmers/java/level1/옹알이_2.java
+++ src/programmers/java/level1/옹알이_2.java
@@ -88,3 +88,16 @@ solution 함수를 완성해주세요.
             }
         }
         return answer;*/
+
+//s3
+/*        int answer = 0;
+        String[] filter = {"aya", "ye", "woo", "ma"};
+        for(int i = 0; i < babbling.length;i++){
+            for(int j = 0; j < filter.length; j++){
+                if(!babbling[i].contains("ayaaya") && !babbling[i].contains("yeye") &&!babbling[i].contains("woowoo")&&!babbling[i].contains("mama"))
+                babbling[i] = babbling[i].replace(filter[j]," ");
+            }
+            babbling[i] = babbling[i].replace(" ", "");
+            if(babbling[i] == "")   answer ++;
+        }
+        return answer;*/
